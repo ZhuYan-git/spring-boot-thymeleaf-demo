2147ca333f8f4ae43603d53db2856f63c82f2099
yanzhu <yanzhu@163.com>
2020-09-27 15:53:49 +0800

    这个是我第一次添加的内容

---
+++ src/main/java/com/offcn/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.offcn.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ：yz
+ * @date ：Created in 2020/9/27 15:48
+ * @version: 1.0
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
