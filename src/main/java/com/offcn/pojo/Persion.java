d2f11790b2bfb001d4498fd0068fd216796ef3ba
yz-offcn <yz@163.com>
2020-12-07 10:12:09 +0800

    新添加文件，测试冲突

---
+++ src/main/java/com/offcn/pojo/Persion.java
@@ -0,0 +1,13 @@
+package com.offcn.pojo;
+
+/**
+ * @author ：yz
+ * @date ：Created in 2020/12/7 9:59
+ * @version: 1.0
+ */
+public class Persion {
+
+    private int id;
+    private String  name;
+    private int age;
+}
